b4c23ad6e82b17edd47c17964ec7ae005df12a38
Oscar Wang <oscarwang22396@gmail.com>
2023-04-19 02:49:25 -0400

    added all pages

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
-import mail_icon from "./mail-icon.svg";
+import { BsEnvelope, BsTelephone } from "react-icons/bs";
+import "./contact.css";
 
 const Contact: React.FC = () => {
-  return <div className="contact"></div>;
+  return (
+    <div className="contact">
+      <div className="contact-card">
+        <div className="contact-card-header">
+          <h1>Contact the Milstein Program</h1>
+        </div>
+        <div className="contact-card-body">
+          <div className="contact-card-body-left">
+            <div className="contact-card-body-left-item">
+              <div className="contact-option">
+                <p>
+                  <BsEnvelope />
+                  <a href="mailto:milsteinprogram@cornell.edu">
+                    milsteinprogram@cornell.edu
+                  </a>
+                </p>
+              </div>
+              <div className="contact-option">
+                <p>
+                  <BsTelephone />
+                  <a href="tel:607-255-5200">607-255-3851</a>
+                </p>
+              </div>
+              <div className="contact-option">
+                <p>
+                  <BsTelephone />
+                  <a href="tel:607-255-1026">607-255-1026</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import photo from "./group-dinner-2.svg";
 import "./index.css";
 
